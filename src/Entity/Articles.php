6465daca7cbed4eedb94c96abea2f9b63c71e3cd
tisoso92170 <dimitri92170@gmail.com>
2019-12-20 04:15:15 +0100

    add authentification

--- src/Entity/Articles.php
+++ src/Entity/Articles.php
@@ -2,9 +2,10 @@
 
 namespace App\Entity;
 
-use Doctrine\Common\Collections\ArrayCollection;
-use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\Common\Collections\ArrayCollection;
+use Symfony\Component\Validator\Constraints\NotBlank;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\ArticlesRepository")
